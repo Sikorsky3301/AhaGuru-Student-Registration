cfbb39b5779cf288b474ca853501b9df88dde780
Rishi Raj <101174357+Sikorsky3301@users.noreply.github.com>
2026-01-21 18:11:27 +0530

    Add root URL redirect to registration page

--- core/students/urls.py
+++ core/students/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 app_name = 'students'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='register/', permanent=False), name='home'),
     path('register/', views.register_student, name='register_student'),
     path('success/', views.registration_success, name='registration_success'),
 ]
